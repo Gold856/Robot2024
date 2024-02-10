2498d33ebf89364894daec49a71380140289fc6a
Gabriel West <gabedwest@gmail.com>
2024-02-10 14:11:19 -0500

    add sensor using command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,15 +9,13 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import frc.robot.Constants.ControllerConstants;
-import frc.robot.Constants.ControllerConstants.Axis;
-import frc.robot.Constants.ControllerConstants.Button;
-import frc.robot.commands.drive.BangBangDriveDistance;
-import frc.robot.commands.drive.DriveDistanceCommand;
-import frc.robot.commands.drive.PIDTurnCommand;
-import frc.robot.commands.drive.SetSteering;
+import frc.robot.commands.indexer.IndexWithSensorCommand;
+import frc.robot.commands.indexer.IndexerCommand;
+import frc.robot.commands.indexer.IndexerStopCommand;
 import frc.robot.subsystems.ArduinoSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IndexerSubsystem;
@@ -46,13 +44,19 @@ public class RobotContainer {
 
 	public RobotContainer() {
 		// Configure the button bindings
-		m_autoSelector.addOption("Test Steering", SetSteering.getCalibrationCommand(m_driveSubsystem));
-		m_autoSelector.addOption("PID Turn 90 degrees", new PIDTurnCommand(m_driveSubsystem, 90, 0.5));
-		m_autoSelector.addOption("Bang Bang Drive 2 Meters", new BangBangDriveDistance(m_driveSubsystem, 2, 0.01));
-		m_autoSelector.addOption("PID Drive 2 Meters", DriveDistanceCommand.create(m_driveSubsystem, 3.0, 0.01));
-		m_autoSelector.addOption("Knock Over Blocks",
-				CommandComposer.getBlocksAuto(m_driveSubsystem));
 
+		/*
+		 * m_autoSelector.addOption("Test Steering",
+		 * SetSteering.getCalibrationCommand(m_driveSubsystem));
+		 * m_autoSelector.addOption("PID Turn 90 degrees", new
+		 * PIDTurnCommand(m_driveSubsystem, 90, 0.5));
+		 * m_autoSelector.addOption("Bang Bang Drive 2 Meters", new
+		 * BangBangDriveDistance(m_driveSubsystem, 2, 0.01));
+		 * m_autoSelector.addOption("PID Drive 2 Meters",
+		 * DriveDistanceCommand.create(m_driveSubsystem, 3.0, 0.01));
+		 * m_autoSelector.addOption("Knock Over Blocks",
+		 * CommandComposer.getBlocksAuto(m_driveSubsystem));
+		 */
 		SmartDashboard.putData(m_autoSelector);
 		configureButtonBindings();
 	}
@@ -64,52 +68,59 @@ public class RobotContainer {
 	 * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
 	 */
 	private void configureButtonBindings() {
-		// new Trigger(() -> DriverStation.getMatchTime() >= 20)
-		// .onTrue(m_ArduinoSubsystem.writeStatus(StatusCode.RAINBOW_PARTY_FUN_TIME));
-		m_driveSubsystem.setDefaultCommand(m_driveSubsystem.driveCommand(
-				() -> m_driverController.getRawAxis(Axis.kLeftY),
-				() -> m_driverController.getRawAxis(Axis.kLeftX),
-				() -> m_driverController.getRawAxis(Axis.kRightTrigger),
-				() -> m_driverController.getRawAxis(Axis.kLeftTrigger)
 
-		));
-
-		m_driverController.button(Button.kCircle).onTrue(m_driveSubsystem.resetHeadingCommand());
-		m_driverController.button(Button.kTriangle).onTrue(m_driveSubsystem.alignModulesToZeroComamnd());
-		m_driverController.button(Button.kSquare).onTrue(m_driveSubsystem.resetEncodersCommand());
-		m_driverController.button(Button.kX).onTrue(new WaitCommand(0.2) {
-			@Override
-			public void initialize() {
-				super.initialize();
-				m_driveSubsystem.setModuleStates(0.1, 0, 0, false);
-			}
-
-			@Override
-			public void execute() {
-				super.execute();
-				m_driveSubsystem.setModuleStates(0.0, 0.1, 0, false);
-			}
-
-			@Override
-			public void end(boolean b) {
-				super.end(b);
-				m_driveSubsystem.setModuleStates(0.0, 0.1, 0, false);
-			}
-		});
-
-		// // Indexer Button Mappings
-		// m_operatorController.button(Button.kSquare).onTrue(new
-		// IndexerFowardCommand(0.5)); // TODO add constants
-		// m_operatorController.button(Button.kCircle).onTrue(new
-		// IndexerReverseCommand(0.5));
-		// m_operatorController.button(Button.kTrackpad).onTrue(new
-		// IndexerStopCommand());
-		// m_operatorController.button(Button.kRightBumper).onTrue(new
-		// IndexerShootCommand(.5, .5));
-
-	}
+	// new Trigger(() -> DriverStation.getMatchTime() >= 20)
+	//
+	// .onTrue(m_ArduinoSubsystem.writeStatus(StatusCode.RAINBOW_PARTY_FUN_TIME));
+	// m_driveSubsystem.setDefaultCommand(m_driveSubsystem.driveCommand(
+	// () -> m_driverController.getRawAxis(Axis.kLeftY),
+	// () -> m_driverController.getRawAxis(Axis.kLeftX),
+	// () -> m_driverController.getRawAxis(Axis.kRightTrigger),
+	// () -> m_driverController.getRawAxis(Axis.kLeftTrigger)
+	// ));
+	// m_driverController.button(Constants.Button.kCircle).onTrue(m_driveSubsystem.
+	// resetHeadingCommand());
+	// m_driverController.button(Constants.Button.kTriangle).onTrue(m_driveSubsystem.
+	// alignModulesToZeroComamnd());
+	// m_driverController.button(Constants.Button.kSquare).onTrue(m_driveSubsystem.
+	// resetEncodersCommand());
+	// m_driverController.button(Constants.Button.kX).onTrue(new WaitCommand(0.2) {
+	// @Override
+	// public void initialize() {
+	// super.initialize();
+	// m_driveSubsystem.setModuleStates(0.1, 0, 0, false);
+	// }
+	// @Override
+	// public void execute() {
+	// super.execute();
+	// m_driveSubsystem.setModuleStates(0.0, 0.1, 0, false);
+	// }
+	// @Override
+	// public void end(boolean b) {
+	// super.end(b);
+	// m_driveSubsystem.setModuleStates(0.0, 0.1, 0, false);
+	// }
+	// });
+	// // Indexer Button Mappings
+	// m_operatorController.button(Button.kSquare).onTrue(new
+	// IndexerFowardCommand(0.5)); // TODO add constants
+	// m_operatorController.button(Button.kCircle).onTrue(new
+	// IndexerReverseCommand(0.5));
+	// m_operatorController.button(Button.kTrackpad).onTrue(new
+	// IndexerStopCommand());
+	// m_operatorController.button(Button.kRightBumper).onTrue(new
+	// IndexerShootCommand(.5, .5));
 
 	public Command getAutonomousCommand() {
-		return m_autoSelector.getSelected();
+		return new SequentialCommandGroup(
+				IndexerCommand.getFowardCommand(m_indexerSubsystem),
+				new WaitCommand(5),
+				IndexerCommand.getReverseCommand(m_indexerSubsystem),
+				new WaitCommand(5),
+				new IndexerStopCommand(m_indexerSubsystem),
+				new WaitCommand(5),
+				new IndexWithSensorCommand(m_indexerSubsystem)
+
+		);
 	}
 }
