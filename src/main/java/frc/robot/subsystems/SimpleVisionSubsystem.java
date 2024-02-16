dbdcb66125add0f3a9b4c93f212fb302a97a929e
Gold856 <117957790+Gold856@users.noreply.github.com>
2024-02-16 09:45:16 -0500

    Simplify and document code

--- src/main/java/frc/robot/subsystems/SimpleVisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/SimpleVisionSubsystem.java
@@ -8,9 +8,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SimpleVisionSubsystem extends SubsystemBase {
 	private final DoubleSubscriber m_subscription;
-	private double m_distance;
+	private final MedianFilter m_angleFilter;
 	private double m_angle;
-	private MedianFilter m_angleFilter;
 
 	public SimpleVisionSubsystem() {
 		m_angleFilter = new MedianFilter(10);
@@ -22,43 +21,19 @@ public class SimpleVisionSubsystem extends SubsystemBase {
 
 	@Override
 	public void periodic() {
-		// do the math right now to convert pose into
-		// angle/distance to target
-		updateSavedPositions();
-	}
-
-	// [Tag]x---- [robot points here]
-	// \ | /
-	// \ z |
-	// \ | /
-	// \A|B|
-	// \|/
-	// / ----- /
-	// / robot /
-	// --------
-	// Limelight gives us distances X and z, and angle B
-	// We want to compute A+B as the angle to target
-	//
-	private void updateSavedPositions() {
 		// get the data from limelight
 		var angle = m_subscription.get();
 		// ignore unrealistic answers
-		if (Math.abs(angle) < 45) {
-			m_angle = m_angleFilter.calculate(angle);
-		}
+		m_angle = m_angleFilter.calculate(angle);
 		SmartDashboard.putNumber("limelight angle to turn", m_angle);
-		SmartDashboard.putNumber("limelight distance", m_distance);
 	}
 
-	// When a tag is in frame, returns the distance from the camera to the tag
-	public double getDistance() {
-		return m_distance;
-	}
-
-	// When a tag is in frame, returns:
-	// 0 if the tag is directly in front of camera
-	// an angle < 0 if the tag is left of camera
-	// an angle > 0 if the tag is right of camera
+	/**
+	 * Returns the angle of the camera to the tag. Negative if the tag is left of
+	 * the camera, positive if the tag is right of the camera.
+	 * 
+	 * @return The angle of the camera
+	 */
 	public double getAngle() {
 		return m_angle;
 	}
