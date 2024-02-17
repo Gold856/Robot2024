b181e38b94bab02a1165068d240d9e7104ddc69d
nit0208b <116853623+nit0208b@users.noreply.github.com>
2024-02-17 11:11:03 -0500

    Testing and debugging with SimpleVisionAlignCommand so that it works reliably

--- src/main/java/frc/robot/subsystems/SimpleVisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/SimpleVisionSubsystem.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.filter.MedianFilter;
 import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -8,11 +7,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SimpleVisionSubsystem extends SubsystemBase {
 	private final DoubleSubscriber m_subscription;
-	private final MedianFilter m_angleFilter;
 	private double m_angle;
 
 	public SimpleVisionSubsystem() {
-		m_angleFilter = new MedianFilter(10);
 		// Setup subscription
 		m_subscription = NetworkTableInstance.getDefault().getTable("limelight")
 				.getDoubleTopic("tx")
@@ -22,9 +19,7 @@ public class SimpleVisionSubsystem extends SubsystemBase {
 	@Override
 	public void periodic() {
 		// get the data from limelight
-		var angle = m_subscription.get();
-		// ignore unrealistic answers
-		m_angle = m_angleFilter.calculate(angle);
+		m_angle = m_subscription.get();
 		SmartDashboard.putNumber("limelight angle to turn", m_angle);
 	}
 
