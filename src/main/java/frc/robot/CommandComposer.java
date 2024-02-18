c1a01b620dfb4499d17a90e811a073ca4104230e
nit0208b <116853623+nit0208b@users.noreply.github.com>
2024-02-18 10:25:47 -0500

    ClampedController (trademarked by Team 20) and TwoScoreRightAuto fixes

--- src/main/java/frc/robot/CommandComposer.java
+++ src/main/java/frc/robot/CommandComposer.java
@@ -5,7 +5,6 @@ import static edu.wpi.first.wpilibj2.command.Commands.*;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.drive.BangBangDriveCommand;
 import frc.robot.commands.drive.BangBangDriveDistanceCommand;
-import frc.robot.commands.drive.SetSteeringCommand;
 import frc.robot.commands.drive.TurnToAngleCommand;
 import frc.robot.subsystems.DriveSubsystem;
 
@@ -39,8 +38,8 @@ public class CommandComposer {
 	 * @return The command.
 	 */
 	public static Command getTwoScoreRightAuto(DriveSubsystem driveSubsystem) {
-		return sequence(driveSubsystem.offsetGyroCommand(-60),
-				new SetSteeringCommand(driveSubsystem, 0),
+		return sequence(
+				// new SetSteeringCommand(driveSubsystem, 0),
 				new BangBangDriveCommand(driveSubsystem, 1.4, 60, 0.01),
 				new TurnToAngleCommand(driveSubsystem, -27, 2, false));
 	}
