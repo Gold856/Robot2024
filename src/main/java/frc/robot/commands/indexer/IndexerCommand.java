d6aa4df6b1248d864d360f1db8c29c94616e2824
IvyCactus <47390086+IvyCactus@users.noreply.github.com>
2024-02-27 10:42:07 -0500

    Fixing an import

--- src/main/java/frc/robot/commands/indexer/IndexerCommand.java
+++ src/main/java/frc/robot/commands/indexer/IndexerCommand.java
@@ -4,7 +4,7 @@
 
 package frc.robot.commands.indexer;
 
-import static frc.robot.Constants.IntakeConstants.*;
+import static frc.robot.Constants.IndexerConstants.*;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IndexerSubsystem;
