6f8392f892159ec3643a553c4a10c8188891b1f7
J Hwang <100035934+jhhbrown1@users.noreply.github.com>
2024-03-09 14:40:43 -0500

    raised max velocity and acceleraion

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -87,8 +87,8 @@ public final class Constants {
 		public static final double kDriveP = 0.4; // up to 1.0?
 		public static final double kDriveI = 0;
 		public static final double kDriveD = 0;
-		public static final double kDriveMaxVelocity = 3; // up to 5?
-		public static final double kDriveMaxAcceleration = 3; // up to 10?
+		public static final double kDriveMaxVelocity = 4; // up to 5?
+		public static final double kDriveMaxAcceleration = 4; // up to 10?
 
 		public static final double kTurnP = 0.02; // was 0.005 upto 0.2?
 		public static final double kTurnI = 0; // was 0.003
