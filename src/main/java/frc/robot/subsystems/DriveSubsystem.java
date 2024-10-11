65d318c89dbd1097d8726b1b9bf23d35840dbb93
Gold856 <117957790+Gold856@users.noreply.github.com>
2024-10-10 20:14:26 -0400

    Increase time to see if it chatters

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -54,7 +54,7 @@ public class DriveSubsystem extends SubsystemBase {
 	/** Creates a new DriveSubsystem. */
 	public DriveSubsystem() {
 		var config = new SysIdRoutine.Config(Units.Volts.of(1.5).per(Units.Seconds.of(1)), Units.Volts.of(4),
-				Units.Seconds.of(4));
+				Units.Seconds.of(5));
 		m_sysidRoutine = new SysIdRoutine(config,
 				new SysIdRoutine.Mechanism((volt) -> {
 					SwerveModuleState[] pos = new SwerveModuleState[4];
