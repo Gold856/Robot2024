d9351739b0f177e68b6bb61016ca38823f6bfb8f
Gold856 <117957790+Gold856@users.noreply.github.com>
2024-12-14 14:01:35 -0500

    Add ramp rate

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -9,6 +9,7 @@ import static frc.robot.Constants.DriveConstants.*;
 import java.util.Arrays;
 import java.util.function.Supplier;
 
+import com.ctre.phoenix6.SignalLogger;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.math.MathUtil;
@@ -53,8 +54,8 @@ public class DriveSubsystem extends SubsystemBase {
 
 	/** Creates a new DriveSubsystem. */
 	public DriveSubsystem() {
-		var config = new SysIdRoutine.Config(Units.Volts.of(1.5).per(Units.Seconds.of(1)), Units.Volts.of(4),
-				Units.Seconds.of(4));
+		var config = new SysIdRoutine.Config(Units.Volts.of(1.75).per(Units.Seconds.of(1)), Units.Volts.of(7),
+				Units.Seconds.of(3), state -> SignalLogger.writeString("state", state.toString()));
 		m_sysidRoutine = new SysIdRoutine(config,
 				new SysIdRoutine.Mechanism((volt) -> {
 					SwerveModuleState[] pos = new SwerveModuleState[4];
